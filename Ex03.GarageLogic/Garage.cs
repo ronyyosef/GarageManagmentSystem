eeea9604879aa602834d4453e8b472891d2a09b1
Omer Arzi <76008630+OmerArzi@users.noreply.github.com>
2021-05-10 19:25:50 +0300

    Update Garage.cs

--- Ex03.GarageLogic/Garage.cs
+++ Ex03.GarageLogic/Garage.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +8,33 @@ namespace Ex03.GarageLogic
 {
     public class Garage
     {
+        public enum eVehicleStatus
+        {
+            InProgress,
+            repaired,
+            paied,
+        }
+        private readonly List<VehicleInTheGarage> VehicleInTheGarageList;
+        public List<string> AllVehiclesLicenseNumberListFiltered(Garage.eVehicleStatus filterBy)
+        {
+            List<string> licenseNumberList = new List<string>();
+            foreach (VehicleInTheGarage vehicleInGarage in VehicleInTheGarageList)
+            {
+                if (vehicleInGarage.Status == filterBy)
+                {
+                    licenseNumberList.Add(vehicleInGarage.GetLicenseNumber());
+                }
+            }
+            return licenseNumberList;
+        }
+        public List<string> AllVehiclesLicenseNumberList()
+        {
+            List<string> licenseNumberList = new List<string>();
+            foreach (VehicleInTheGarage vehicleInGarage in VehicleInTheGarageList)
+            {
+                licenseNumberList.Add(vehicleInGarage.GetLicenseNumber());
+            }
+            return licenseNumberList;
+        }
     }
 }
